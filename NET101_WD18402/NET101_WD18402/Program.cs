1371e72d1fa1c61f7ab7e2b0fe93cd6eabb43b1b
huyennk6 <huyennk6@fpt.edu.vn>
2023-05-08 13:36:23 +0700

    Lam quen voi CSharp

---
+++ NET101_WD18402/NET101_WD18402/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET101_WD18402
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //viết tiếng việt
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+
+            //cw tab tab
+            Console.WriteLine("Hello");
+            Console.Write("FPT");
+            Console.WriteLine("Polytechnic");
+            Console.WriteLine("Hà Nội");
+            Console.WriteLine("Hà Nội");
+            Console.WriteLine("Hà Nội");
+            Console.WriteLine("Nguyễn Khánh Huyền");
+            /* 1 số tổ hợp phím */
+            //ctrl + F5: chạy CT
+            //ctrl + D: copy code
+            //ctrl + K + D: format code
+            //ctrl + K + C: comment code
+            //ctrl + K + U: mở comment code
+        }
+    }
+}
